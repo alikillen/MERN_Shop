de15f222f7f6f5886a20209b46a97e60f8aa085a
alikillen <alisoneme1994@gmail.com>
2020-12-02 22:16:01 +1000

    frontend updates

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { LinkContainer } from "react-router-bootstrap"
 import {Navbar, Nav, Container } from "react-bootstrap"
 
 export default class Header extends Component {
@@ -7,14 +8,23 @@ export default class Header extends Component {
       <header>
         <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">MERN Shop</Navbar.Brand>
+        <LinkContainer to ="/">
+          <Navbar.Brand> MERN Shop</Navbar.Brand>
+        </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i>
+            
+            <LinkContainer to ="/cart">
+            <Nav.Link><i className="fas fa-shopping-cart"></i>
               Cart</Nav.Link>
-              <Nav.Link href="/login"><i className="fas fa-user"></i>
+            </LinkContainer>
+            
+            <LinkContainer to ="/login">
+              <Nav.Link><i className="fas fa-user"></i>
               Login</Nav.Link>
+            </LinkContainer>
+              
             </Nav>         
           </Navbar.Collapse>
           </Container>
